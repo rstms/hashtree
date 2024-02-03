23fac3f580371ef074d93cc840410176be47afba
Matt Krueger <mkrueger@rstms.net>
2024-02-03 05:49:03 -0700

    Bump version: 0.0.3 → 0.0.4

--- hashtree/version.py
+++ hashtree/version.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 __author__ = "Matt Krueger"
 __email__ = "mkrueger@rstms.net"
-__timestamp__ = "2024-02-03 04:05:55-07:00"
+__timestamp__ = "2024-02-03 05:49:03-07:00"
